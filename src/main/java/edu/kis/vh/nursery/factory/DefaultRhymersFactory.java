61dd7d3a1d73f571c4b43e5d98480a92ef7e02e3
snapeeek <maciekwozniak97@gmail.com>
2020-01-03 17:55:43 +0100

    naming branch

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,6 +5,7 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
 //alt i strzałka w prawo lub w lewo przełączają otwarte pliki
+//w sumie podpunkt z nazewnictwem zrobilem juz w branchu format, zagalopowałem się!
 
 public class DefaultRhymersFactory implements Rhymersfactory
 {
